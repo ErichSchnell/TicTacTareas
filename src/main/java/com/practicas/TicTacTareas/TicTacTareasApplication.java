07faaa2a4f3d523c82a5b509ca2363bb77a9733c
SchnellNotebook\Social Learning <erich2128@gmail.com>
2025-08-14 13:31:02 -0300

    first commit

---
+++ src/main/java/com/practicas/TicTacTareas/TicTacTareasApplication.java
@@ -0,0 +1,13 @@
+package com.practicas.TicTacTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicas/TicTacTareas/TicTacTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicas.TicTacTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
